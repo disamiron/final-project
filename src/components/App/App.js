437cd6c89256a386ea1be2768fd9e7d288c8a589
disamiron <mirronovdenis@gmail.com>
2020-02-15 19:54:20 +0300

    final-project
    created react simple app, added routing

---
+++ src/components/App/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import './App.css';
+
+//TODO: fix import
+//import PokemonList from "./PokemonList.js";
+
+const pokemons = [
+    {
+        id: 1,
+        name: "pika"
+    },
+    {
+        id: 2,
+        name: "pika2"
+    }
+];
+
+const Pokemon = (props) => (
+    <div>
+        <span>{props.name}</span>
+        <span>{props.id}</span>
+    </div>
+);
+    
+
+class PokemonList extends Component {
+    render() {
+        return (
+            <div>
+            <h1>POKEMONS LIST:</h1>
+            <div>
+                {pokemons.map(pokemon => (
+                    <Pokemon
+                        name={pokemon.name}
+                        id={pokemon.id}
+                    />
+                ))}
+            </div>
+            </div>
+        );
+    }
+};
+
+class App extends Component {
+    render() {
+        return (
+        <Router>
+            <h2>dkjfn;</h2>
+            <Switch>
+                <Route exact path="/" component={PokemonList} />
+                <Route exact path="/pokemon">
+                    <h1>Pokemon</h1>
+                </Route>
+            </Switch>
+    </Router>
+        );
+    }
+}
+
+
+
+export default App;
